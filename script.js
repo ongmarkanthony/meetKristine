a61c7068294f287b12401a59f801a314ebc01e14
Mao <ongmarkanthony@gmail.com>
2023-07-12 14:24:56 +0800

    create login page

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 
-// Function to handle login
 const login = () => {
-    // Get the values entered by the user
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
   
-    // Make an API request to verify the login credentials
-    // Replace 'api/login' with the actual endpoint of your login API
     fetch(' https://my.api.mockaroo.com/mk_users.json?key=0339a140', {
       method: 'POST',
       body: JSON.stringify({ username: username, password: password }),
@@ -16,11 +12,8 @@ const login = () => {
     })
       .then((response) => {
         if (response.ok) {
-          // Successful login
           alert('Login successful!');
-          // You can perform further actions here, such as redirecting to a dashboard page
         } else {
-          // Failed login
           alert('Login failed. Please check your credentials.');
         }
       })
@@ -46,7 +39,6 @@ function fetchUserData() {
         })
         .then((data) => {
             document.querySelector('#userContainer').innerHTML = `
-            <img src="${data.results[0].picture.large}">
             <h4 class="fullName">${data.results[0].fullName} </h4>
             <h4 class="jobTitle">${data.results[0].job.title}</h4>
             <h4 class="email">${data.results[0].email}</h4>
